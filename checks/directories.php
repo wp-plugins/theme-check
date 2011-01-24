0e872d2483b6b0bc636abcf7f3297d09f5501cd4
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-24 23:12:02 +0000

    20101228.3 tagged
    git-svn-id: https://plugins.svn.wordpress.org/theme-check/tags/20101228.3@336726 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/directories.php
+++ checks/directories.php
@@ -3,28 +3,28 @@
 class DirectoriesCheck implements themecheck {
 	protected $error = array();
 
-	function check( $php_files, $css_files, $other_files) {
+	function check( $php_files, $css_files, $other_files ) {
 
 		$ret = true;
 		$found = false;
 
 		foreach ( $php_files as $name => $file ) {
 			checkcount();
-			if ( strpos($name, '.git') !== false || strpos($name, '.svn') !== false ) $found = true;
+			if ( strpos( $name, '.git' ) !== false || strpos( $name, '.svn' ) !== false ) $found = true;
 		}
 
 		foreach ( $css_files as $name => $file ) {
 			checkcount();
-			if ( strpos($name, '.git') !== false || strpos($name, '.svn') !== false ) $found = true;
+			if ( strpos( $name, '.git' ) !== false || strpos( $name, '.svn' ) !== false ) $found = true;
 		}
 
 		foreach ( $other_files as $name => $file ) {
 			checkcount();
-			if ( strpos($name, '.git') !== false || strpos($name, '.svn') !== false ) $found = true;
+			if ( strpos( $name, '.git' ) !== false || strpos( $name, '.svn' ) !== false ) $found = true;
 		}
 
 		if ($found) {
-			$this->error[] = "<span class='tc-lead tc-required'>REQUIRED</span>: " . __( 'Please remove any extraneous directories like .git or .svn from the ZIP file before uploading it.', 'theme-check' );
+			$this->error[] = "<span class='tc-lead tc-required'>REQUIRED</span>: " . __( 'Please remove any extraneous directories like .git or .svn from the ZIP file before uploading it.', 'themecheck' );
 			$ret = false;
 		}
 
@@ -33,5 +33,4 @@ class DirectoriesCheck implements themecheck {
 
 	function getError() { return $this->error; }
 }
-
 $themechecks[] = new DirectoriesCheck;
